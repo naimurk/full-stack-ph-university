82f4f875b03097019122b1644722314ee811f138
unknown <naimurk781@gmail.com>
2024-03-05 09:53:07 +0600

    custom  basequery

--- ph-client/src/pages/Admin/AcademicManagement/AcademicSemester/AcademicSemester.tsx
+++ ph-client/src/pages/Admin/AcademicManagement/AcademicSemester/AcademicSemester.tsx
@@ -1,13 +1,9 @@
 import { useGetAllAcademicSemesterQuery } from "../../../../redux/feature/AcademicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
-    const {data} = useGetAllAcademicSemesterQuery(undefined);
-    console.log(data)
-    return (
-        <div>
-            AcademicSemester
-        </div>
-    );
+  const { data } = useGetAllAcademicSemesterQuery(undefined);
+  console.log(data);
+  return <div>AcademicSemester</div>;
 };
 
 export default AcademicSemester;
--- ph-client/src/redux/feature/AcademicSemester/academicSemesterApi.ts
+++ ph-client/src/redux/feature/AcademicSemester/academicSemesterApi.ts
@@ -1,15 +1,14 @@
 import { baseApi } from "../../api/baseApi";
 
 const academicSemesterApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllAcademicSemester: builder.query({
-          query: () => ({
-            url: "/academic-semesters",
-            method: "GET",
-
-          }),
-        }),
+  endpoints: (builder) => ({
+    getAllAcademicSemester: builder.query({
+      query: () => ({
+        url: "/academic-semesters",
+        method: "GET",
       }),
-})
+    }),
+  }),
+});
 
-export const {useGetAllAcademicSemesterQuery } = academicSemesterApi
+export const { useGetAllAcademicSemesterQuery } = academicSemesterApi;
