92aac09041fcf3c68ffc57a1cb632cd350f2999e
unknown <naimurk781@gmail.com>
2024-03-06 10:53:48 +0600

    s

--- ph-client/src/redux/api/baseApi.ts
+++ ph-client/src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../feature/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -13,10 +14,24 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args , api , extraOptions) => {
-  const result = await baseQuery(args , api , extraOptions)
-  console.log(result)
-}
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const resultOfRefreshToken = await res.json();
+    const user = (api.getState() as RootState).auth.user;
+    api.dispatch(
+      setUser({
+        user,
+        token: resultOfRefreshToken?.data?.accessToken,
+      })
+    );
+  }
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
