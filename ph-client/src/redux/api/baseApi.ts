eecf4fdd4d08592349cf513055e269001df76f66
unknown <naimurk781@gmail.com>
2024-03-06 12:50:27 +0600

    s

--- ph-client/src/redux/api/baseApi.ts
+++ ph-client/src/redux/api/baseApi.ts
@@ -1,6 +1,7 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../feature/auth/authSlice";
+import { logout, setUser } from "../feature/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -14,21 +15,27 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+const baseQueryWithRefreshToken:BaseQueryFn<FetchArgs, BaseQueryApi , DefinitionType> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
   if (result?.error?.status === 401) {
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
       credentials: "include",
     });
     const resultOfRefreshToken = await res.json();
-    const user = (api.getState() as RootState).auth.user;
-    api.dispatch(
-      setUser({
-        user,
-        token: resultOfRefreshToken?.data?.accessToken,
-      })
-    );
+    if(resultOfRefreshToken?.data?.accessToken){
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: resultOfRefreshToken?.data?.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    }else {
+      api.dispatch(logout())
+    }
   }
   return result;
 };
@@ -38,4 +45,3 @@ export const baseApi = createApi({
   baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
-  
