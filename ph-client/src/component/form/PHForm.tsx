a820d2beca216d07f037f7328c67132b0002faf9
unknown <naimurk781@gmail.com>
2024-03-09 18:02:00 +0600

    s

--- ph-client/src/component/form/PHForm.tsx
+++ ph-client/src/component/form/PHForm.tsx
@@ -6,22 +6,18 @@ import {
   SubmitHandler,
   useForm,
 } from "react-hook-form";
-
 type TForm = {
   onSubmit: SubmitHandler<FieldValues>;
   children: ReactNode;
 } & TDefaultValues;
-
 type TDefaultValues = {
   defaultValues?: Record<string, any>;
 };
 const PHForm = ({ onSubmit, children, defaultValues }: TForm) => {
   const formConfig: TDefaultValues = {};
-
   if (defaultValues) {
     formConfig["defaultValues"] = defaultValues;
   }
-
   const methods = useForm(formConfig);
   return (
     <FormProvider {...methods}>
