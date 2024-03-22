25756664be7692040b51cef767cdd96e2fc29e44
unknown <naimurk781@gmail.com>
2024-03-22 11:58:05 +0600

    academin management route set

--- ph-client/src/Routes/adminRoutes.tsx
+++ ph-client/src/Routes/adminRoutes.tsx
@@ -1,10 +1,13 @@
+import AcademicDepartment from "../pages/Admin/AcademicManagement/AcademicDepartment/AcademicDepartment";
+import CreateAcademicDepartment from "../pages/Admin/AcademicManagement/AcademicDepartment/CreateAcademicDepartment";
+import AcademicFaulty from "../pages/Admin/AcademicManagement/AcademicFaculty/AcademicFaulty";
+import CreateAcademicFaculty from "../pages/Admin/AcademicManagement/AcademicFaculty/CreateAcademicFaculty";
 import AcademicSemester from "../pages/Admin/AcademicManagement/AcademicSemester/AcademicSemester";
+import CreateAcademicSemester from "../pages/Admin/AcademicManagement/AcademicSemester/CreateAcademicSemester";
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import CreateAdmin from "../pages/Admin/CreateAdmin";
 import CreateFaculty from "../pages/Admin/CreateFaculty";
 import CreateStudent from "../pages/Admin/CreateStudent";
-
-
 
 export const adminPaths = [
   {
@@ -16,11 +19,35 @@ export const adminPaths = [
     name: "Academic Management",
     children: [
       {
+        name: "Create A. Semester",
+        path: "create-academic-semester",
+        element: <CreateAcademicSemester></CreateAcademicSemester>,
+      },
+      {
         name: "Academic Semester",
         path: "academic-semester",
         element: <AcademicSemester></AcademicSemester>,
       },
-    
+      {
+        name: "Create A. faculty",
+        path: "create-academic-faculty",
+        element: <CreateAcademicFaculty></CreateAcademicFaculty>,
+      },
+      {
+        name: "Academic Faculty",
+        path: "academic-faculty",
+        element: <AcademicFaulty></AcademicFaulty>,
+      },
+      {
+        name: "Create A. Department",
+        path: "create-academic-department",
+        element: <CreateAcademicDepartment></CreateAcademicDepartment>,
+      },
+      {
+        name: "Academic Department",
+        path: "Academic Department",
+        element: <AcademicDepartment></AcademicDepartment>,
+      },
     ],
   },
   {
@@ -44,6 +71,3 @@ export const adminPaths = [
     ],
   },
 ];
-
-
-
